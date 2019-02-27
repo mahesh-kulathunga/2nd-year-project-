eb2335fcd9241dac8bf43397acec0bf04eb0c4f1
mahesh-klathnga <maheshbkulathunga@gmail.com>
2019-02-27 22:21:30 +0530

    fist commit

---
+++ database.sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 12, 2017 at 12:26 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_exam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `adminId` int(11) NOT NULL,
+  `adminUser` varchar(50) NOT NULL,
+  `adminPass` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`adminId`, `adminUser`, `adminPass`) VALUES
+(1, 'admin', '202cb962ac59075b964b07152d234b70');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_ans`
+--
+
+CREATE TABLE `tbl_ans` (
+  `id` int(11) NOT NULL,
+  `quesNo` int(11) NOT NULL,
+  `rightAns` int(11) NOT NULL DEFAULT '0',
+  `ans` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_ans`
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_ques`
+--
+
+CREATE TABLE `tbl_ques` (
+  `id` int(11) NOT NULL,
+  `quesNo` int(11) NOT NULL,
+  `ques` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_ques`
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `userId` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_viva`
+--
+
+CREATE TABLE `tbl_viva` (
+  `id` int(200) NOT NULL,
+  `name` varchar(2000) NOT NULL,
+  `email` varchar(2000) NOT NULL,
+  `facebook` varchar(2000) NOT NULL,
+  `skype` varchar(2000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_viva`
+--
+
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`adminId`);
+
+--
+-- Indexes for table `tbl_ans`
+--
+ALTER TABLE `tbl_ans`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_ques`
+--
+ALTER TABLE `tbl_ques`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`userId`);
+
+--
+-- Indexes for table `tbl_viva`
+--
+ALTER TABLE `tbl_viva`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `adminId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `tbl_ans`
+--
+ALTER TABLE `tbl_ans`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+--
+-- AUTO_INCREMENT for table `tbl_ques`
+--
+ALTER TABLE `tbl_ques`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `tbl_viva`
+--
+ALTER TABLE `tbl_viva`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
